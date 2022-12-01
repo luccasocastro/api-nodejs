78962145cafa3ed161aa652a931adaeb5c5e93a0
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-12-01 16:52:56 -0300

    get all carros

--- src/controllers/CarroController.js
+++ src/controllers/CarroController.js
@@ -1,1 +1,17 @@
 const CarroService = require('../services/CarroService');
+
+module.exports = {
+    findAll: async (req,res) => {
+        let json = {error:'', result:[]};
+
+        let carros = await CarroService.findAll();
+
+        for(let i in carros){
+            json.result.push({
+                codigo: carros[i].codigo,
+                descricao: carros[i].modelo
+            });
+        }
+        res.json(json);
+    }
+}
--- src/services/CarroService.js
+++ src/services/CarroService.js
@@ -1,5 +1,12 @@
 const db = require('../db');
 
 module.exports = {
-
+    findAll: () => {
+        return new Promise((aceito, rejeitado) => {
+            db.query('SELECT * FROM carros', (error, results)=>{
+                if(error) {rejeitado(error); return;}
+                aceito(results);
+            });
+        });
+    }
 };
