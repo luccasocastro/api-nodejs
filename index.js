3b28fdfaa1f8eb0f48bfbeb59b245e02576d7260
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-12-01 15:06:29 -0300

    criando base da API

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const server = express();
+
+server.get('/', (req,res)=>{
+    return res.json({mensagem: 'Nossa API está funcionando!'})
+})
+
+server.listen(3000, ()=>{
+    console.log('servidor está funcionando...')
+});
